54a9eef9be433e7d541762fc7c6c38a096c9d026
m0rphism <m0rphism@users.noreply.github.com>
2016-09-11 16:03:05 +0200

    Added example code.

--- example/Main.hs
+++ example/Main.hs
@@ -1,1 +1,22 @@
+{-# LANGUAGE LambdaCase, UnicodeSyntax #-}
+
+import Printcess.PrettyPrinting
+
+type Ident = String
+
+data Expr
+  = EVar Ident
+  | EAbs Ident Expr
+  | EApp Expr Expr
+
+instance Pretty Expr where
+  pp = \case
+    EVar i     → pp i
+    EAbs i e   → assocR 0 $ "λ" +> i +> ". " +> R e
+    EApp e1 e2 → assocL 9 $ L e1 +> " " +> R e2
+
+expr0 = EApp (EVar "x") (EVar "y")
+expr1 = EAbs "x" $ EAbs "y" $ EApp expr0 expr0
+test1 = putStrLn $ pretty expr1
+
 main = putStrLn "Printcess Pretty Printing Example"
