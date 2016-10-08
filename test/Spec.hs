8e58dbf6327fdc562e493331da018f9ea50373f0
m0rphism <m0rphism@users.noreply.github.com>
2016-10-08 15:36:04 +0200

    Update.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,18 @@
 import Test.Hspec
 import Printcess.PrettyPrinting
+import Control.Lens
 
 main :: IO ()
 main = hspec $ do
   describe "printcess" $ do
     it "prints a string" $
       pretty def "foo" `shouldBe` "foo"
+    it "breaks too long lines" $
+      pretty (def & configMaxLineWidth .~ 11) "foo bar baz boo"
+        `shouldBe` "foo bar \n    baz boo"
+    it "breaks too long lines twice" $
+      pretty (def & configMaxLineWidth .~ 11) "foo bar baz boo r"
+        `shouldBe` "foo bar \n    baz boo \n    r"
+    it "breaks too long lines twice" $
+      pretty (def & configMaxLineWidth .~ 11) "foo bar baz boo raz roo"
+        `shouldBe` "foo bar \n    baz boo \n    raz roo"
